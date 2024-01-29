f5d18051d7f18030a8d24cff12999f9ad552414e
Chibi <chibi@striim.com>
2024-01-29 17:35:15 +0530

    Infra Refactor and comments added

--- modules/lb/main.tf
+++ modules/lb/main.tf
@@ -1,32 +1,9 @@
-resource "aws_security_group" "this" {
-  name        = "${var.project}-lb-secgroup"
-  description = "Traffic rules"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    description = "Allow HTTP"
-    from_port   = "80"
-    to_port     = "80"
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description      = "Allow all outbound traffic"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
-
-
+#LB - START
 resource "aws_lb" "this" {
   name               = "${var.project}-vpc"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.this.id]
+  security_groups    = var.secgroup_ids
   enable_deletion_protection = false
 
   subnets = var.subnet_ids
@@ -38,7 +15,7 @@ resource "aws_lb" "this" {
   idle_timeout = 60
 }
 
-
+# TG - START
 resource "aws_lb_target_group" "this" {
   name     = "${var.project}-tg"
   port     = 80
@@ -47,15 +24,12 @@ resource "aws_lb_target_group" "this" {
   vpc_id = var.vpc_id
 }
 
-# locals {
-#   instance_tags = { for instance in var.instance_ids : instance => instance.tags }
-# }
-
 resource "aws_lb_target_group_attachment" "this" {
   for_each = { for idx, id in var.instance_ids : idx => id }
   target_group_arn = aws_lb_target_group.this.arn
   target_id        = each.value
 }
+# TG - END
 
 resource "aws_lb_listener" "web" {
   load_balancer_arn = aws_lb.this.arn
@@ -67,3 +41,5 @@ resource "aws_lb_listener" "web" {
     target_group_arn = aws_lb_target_group.this.arn
   }
 }
+
+# LB - END
--- modules/lb/output.tf
+++ modules/lb/output.tf
@@ -1,4 +1,4 @@
-# output "vm_ip" {
-#   description = "Public IP of the LB"
-#   value = aws_lb.this.public_ip
-# }
+output "lb_dns" {
+  description = "Domain for the LB"
+  value = aws_lb.this.dns_name
+}
